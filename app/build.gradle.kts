dc94fca1a911b69bf97b88a765050f267b5ac200
ggneteufa <112540745+ggneteufa@users.noreply.github.com>
2024-01-18 14:40:24 +0700

    class bottom bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    id("kotlin-parcelize")
+
 }
 
 android {
@@ -59,6 +62,20 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation ("androidx.compose.material3:material3:1.2.0-alpha04")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
+    implementation("androidx.navigation:navigation-compose:2.5.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation ("androidx.navigation:navigation-compose:2.7.2")
+
+    implementation ("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.compose.material3:material3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
