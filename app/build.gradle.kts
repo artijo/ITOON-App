638a5c33ae8b592630c5b8359df3a2fb267677d3
CocoaTony <113881404+CocoaTony@users.noreply.github.com>
2024-02-11 18:57:24 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -66,26 +66,25 @@ dependencies {
     //Ohm
     implementation ("com.google.accompanist:accompanist-pager:0.12.0")
     implementation("io.coil-kt:coil-compose:2.1.0")
-    implementation ("androidx.compose.material3:material3:1.2.0-alpha04")
-    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
-    implementation("androidx.compose.foundation:foundation:1.5.4")
-    implementation("androidx.navigation:navigation-compose:2.5.7")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.7")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
-    implementation ("androidx.navigation:navigation-compose:2.7.2")
-
+    implementation ("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
+    implementation("androidx.compose.foundation:foundation:1.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 //  Corouttines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
 
-    implementation ("com.google.code.gson:gson:2.8.7")
+    implementation ("com.google.code.gson:gson:2.10")
 
 
 
@@ -108,7 +107,7 @@ dependencies {
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("io.coil-kt:coil-compose:1.3.0")
+    implementation("io.coil-kt:coil-compose:2.1.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
 }
