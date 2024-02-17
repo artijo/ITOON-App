86584307cf7fa3d33de05aca17997b5eeed61548
ggneteufa <112540745+ggneteufa@users.noreply.github.com>
2024-02-17 15:18:31 +0700

    Search Page Success(search field text does at top bar)

--- app/src/main/java/com/project/itoon/NavBottomBar/Search.kt
+++ app/src/main/java/com/project/itoon/NavBottomBar/Search.kt
@@ -1,7 +1,12 @@
 package com.project.itoon.NavBottomBar
 
 import android.content.Context
+import android.util.Log
+import android.view.KeyEvent
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +18,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
@@ -31,16 +37,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
 import com.project.itoon.LoginAndSignUp.User
 import com.project.itoon.firstpageapi.Cartoon
 import com.project.itoon.firstpageapi.CartoonAPI
@@ -50,6 +63,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchPage(navHostController: NavHostController){
     val createClient = CartoonAPI.create()
@@ -61,7 +75,7 @@ fun SearchPage(navHostController: NavHostController){
 
     val lifecycleOwner = LocalLifecycleOwner.current
     val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
-
+//    val (focusRequester) = FocusRequester.createRefs() ////Keyboard Action
     LaunchedEffect(lifecycleState) {
         when (lifecycleState) {
             Lifecycle.State.DESTROYED -> {}
@@ -91,50 +105,51 @@ fun SearchPage(navHostController: NavHostController){
                     .padding(10.dp),
                 value = textFieldName,
                 onValueChange = { textFieldName = it },
-                label = { Text(text = "Cartoon name") }
-            )
+                label = { Text(text = "Cartoon name") },
+                trailingIcon =  {Icon(imageVector = Icons.Default.Search, contentDescription = null,
+                    Modifier.clickable {
+                        if (textFieldName.trim().isEmpty()) {
+                            showAllCartoon(cartoonItemsList, contextForToast)
+                        } else {
+                            cartoonItemsList.clear()
+                            createClient.SearchCartoon(textFieldName)
+                                .enqueue(object : Callback<List<Cartoon>> {
+                                    override fun onResponse(
+                                        call: Call<List<Cartoon>>,
+                                        response: Response<List<Cartoon>>
+                                    ) {
+                                        if (response.isSuccessful) {
+                                            Toast.makeText(
+                                                contextForToast,
+                                                "${response!!.body()?.size}", Toast.LENGTH_LONG
+                                            ).show()
+                                            response!!.body()?.forEach {
+                                                Log.i("Data",it.toString())
+                                                cartoonItemsList.add(it)
+                                            }
 
-            Button(onClick = {
-                if (textFieldName.trim().isEmpty()) {
-                    showAllCartoon(cartoonItemsList, contextForToast)
-                } else {
-                    cartoonItemsList.clear()
-                    createClient.SearchCartoon(textFieldName)
-                        .enqueue(object : Callback<List<Cartoon>> {
-                            override fun onResponse(
-                                call: Call<List<Cartoon>>,
-                                response: Response<List<Cartoon>>
-                            ) {
-                                if (response.isSuccessful) {
-                                    Toast.makeText(
-                                        contextForToast,
-                                        "${response!!.body()}", Toast.LENGTH_LONG
-                                    ).show()
+                                        } else {
+                                            Toast.makeText(
+                                                contextForToast,
+                                                "Cartoon not Found", Toast.LENGTH_LONG
+                                            ).show()
+                                        }
+                                    }
+
+                                    override fun onFailure(call: Call<List<Cartoon>>, t: Throwable) {
+                                        Toast.makeText(
+                                            contextForToast, "Error onFailure at search " + t.message,
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                })
+                        }
+                    })
+                    }
+                )}
 
 
-                                } else {
-                                    Toast.makeText(
-                                        contextForToast,
-                                        "Cartoon not Found", Toast.LENGTH_LONG
-                                    ).show()
-                                }
-                            }
 
-                            override fun onFailure(call: Call<List<Cartoon>>, t: Throwable) {
-                                Toast.makeText(
-                                    contextForToast, "Error onFailure at search " + t.message,
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        })
-                }
-            })
-            {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "Search", tint = Color.White)
-            }
-
-
-        }
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
@@ -148,12 +163,9 @@ fun SearchPage(navHostController: NavHostController){
                         modifier = Modifier
                             .padding(horizontal = 8.dp, vertical = 8.dp)
                             .fillMaxWidth()
-                            .height(130.dp),
+                            .height(100.dp),
                         colors = CardDefaults.cardColors(
                             containerColor = Color.White,
-                        ),
-                        elevation = CardDefaults.cardElevation(
-                            defaultElevation = 2.dp
                         ),
                         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
                         onClick = {
@@ -166,14 +178,28 @@ fun SearchPage(navHostController: NavHostController){
                             Modifier
                                 .fillMaxWidth()
                                 .height(Dp(value = 130f))
-                                .padding(16.dp),
+                                .padding(16.dp)
+                            ,
                             verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Text(
-                                text = "Name: ${item.name}\n" +
-                                        "Description: ${item.description}\n",
-                                Modifier.weight(0.85f)
+                            Image(
+                                painter = rememberAsyncImagePainter(item.thumbnail),
+                                contentDescription = item.name,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .width(60.dp)
+                                    .height(60.dp)
+                                    .clip(RoundedCornerShape(10.dp))
                             )
+
+                            Text(
+                                text = "${item.name}\n" ,
+                                Modifier.weight(0.85f).padding(start = 10.dp),
+                                textAlign = TextAlign.Justify,
+                                fontSize = 15.sp
+                            )
+
+
 
                         }
                     }
