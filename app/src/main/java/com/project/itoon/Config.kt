bcc502f8c28d00e409f5c2f9e9a6eff971b37182
tijo <81065469+artijo@users.noreply.github.com>
2024-02-22 21:25:05 +0700

    Update Config.kt

--- app/src/main/java/com/project/itoon/Config.kt
+++ app/src/main/java/com/project/itoon/Config.kt
@@ -2,5 +2,5 @@ package com.project.itoon
 
 data class Config(
     val APIBaseUrl: String = "https://itoon-backend.artijo.com/",
-    val WebClientBaseUrl: String = "https://itoonclient.artijo.com/"
+    val WebClientBaseUrl: String = "https://itoonclient.artijo.com"
 )
